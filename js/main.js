62da78f974ec62f97c0f65fea374e51d0fb6f3bb
Pedro Marins <contato@pedromarins.com>
2022-02-18 03:31:07 -0300

    video 5.2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
 const controle = document.querySelectorAll("[data-controle]")
+const estastiticas = document.querySelectorAll("[data-estatistica]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -4
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 43
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.peca)
     })
 })
 
@@ -16,27 +50,8 @@ function manipulaDados(operacao, controle) {
     }
 }
 
-
-{
-bracos {
-    forca
-    poder
-    energia
-    velocidade
+function atualizaEstatisticas(peca) {
+    estastiticas.forEach( (elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    }) 
 }
-blindagem {
-    forca
-    poder
-    energia
-    velocidade
-}
-nucleos
-pernas
-foguetes
-}
-
-
-forca
-poder
-energia
-velocidade
